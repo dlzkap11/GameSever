4ed78e33cf02569caa7cfa07c1e8b6ff7c17e65b
dlzkap11 <dlzkap11@gmail.com>
2024-08-27 03:24:47 +0900

    AutoResetEvent - Event를 이용해 lock이 풀린지를 알려줌 -> 기존 lock보다 비용이 더   발생해서 느린편(대신 lock이 풀리자마자 바로 다음 쓰레드가 실행).. 근데 Mutex가 더 오래걸림(대신 더 많은 값들을   사용함, 쓰레드 아이디, 몇번 잠금을 했는지 카운팅등을 해줌)

--- SeverCore/Program.cs
+++ SeverCore/Program.cs
@@ -16,45 +16,26 @@ namespace SeverCore
     // SpinLock -> 대기상태에서 무한으로 뺑뺑이 도는 락(무작정 기다리기때문에 CPU점유율이 확 튀는 현상이 발생 할 수 있음...)
     class Lock
     {
-        volatile int _locked = 0; 
+        //bool <- 커널
+        AutoResetEvent _available = new AutoResetEvent(true);
+
+        //ManualResetEvent _available_2 = new ManualResetEvent(true); //lock구현시에는 잘 안씀..
 
         public void Acquire()
         {
-            while (true)
-            {
-                //int original = Interlocked.Exchange(ref _locked, 1);
-                //if (original == 0)
-                //    break;
-                //{
-                //    int original = _locked;
-                //    _locked = 1;
-                //    if (original == 0)
-                //        break;
-                //}
+            _available.WaitOne(); // 입장 시도
+            //_available.Reset(); //bool = false(WaitOne에서 알아서 해줌 Auto)
 
-                // CAS Compare-And-Swap
-                int expected = 0; //예상하는 값
-                int desired = 1;  //원하는 값
-                if(Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
-                    break;
-                //{
-                //    if(_locked == 0)
-                //        _locked = 1;
-                //}
+            //_available_2.WaitOne();
+            //_available_2.Reset(); //문을 닫는다.
 
-                //쉬는 시간
-                //Thread.Sleep(1); // 무조건 휴식 -> 1ms 쉬겠다 희망
-                //Thread.Sleep(0); // 조건부 양보 -> 나보다 우선순위가 낮은 애들한테는 양보 불가 -> 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테 강약약강
-                Thread.Yield();  // 관대한 양보 -> 관대하게 양보, 지금 실행이 가능한 쓰레드가 있으면 실행해라 -> 실행 가능 쓰레드가 없으면 본인이 시간 소진
-
-
-            }
-            
         }
 
         public void Release()
         {
-            _locked = 0; 
+            _available.Set();  //flag = true
+
+            //_available_2.Set();
         }
     }
 
@@ -62,25 +43,40 @@ namespace SeverCore
     {
         static int _num = 0;
         static Lock _lock = new Lock();
+        static Mutex _lock_2 = new Mutex(); //커널동기화객체..
+
+        // bool AutoResetEvent 
+
+
+        // int ThreadId  Mutex -> 좀 더 비용이 많이 듬
+
 
         static void Thread_1()
         {
-            for(int i = 0; i < 1000000; i++)
+            for(int i = 0; i < 10000; i++)
             {
-                _lock.Acquire(); //들어가기전 확인 및 들어간 후 락을 잠금
+                //_lock.Acquire(); //들어가기전 확인 및 들어간 후 락을 잠금
+                //_num++;
+                //_lock.Release(); //끝나고 락을 풀어줌
+
+                _lock_2.WaitOne();
                 _num++;
-                _lock.Release(); //끝나고 락을 풀어줌
+                _lock_2.ReleaseMutex();
             }
             
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                _lock.Acquire(); //들어가기전 확인 및 들어간 후 락을 잠금
+                //_lock.Acquire(); //들어가기전 확인 및 들어간 후 락을 잠금
+                //_num--;
+                //_lock.Release(); //끝나고 락을 풀어줌
+
+                _lock_2.WaitOne();
                 _num--;
-                _lock.Release(); //끝나고 락을 풀어줌
+                _lock_2.ReleaseMutex();
             }
         }
 
