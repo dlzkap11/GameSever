d7fde47a18bef73a4521065ca4877f15911965e4
dlzkap11 <dlzkap11@gmail.com>
2024-08-28 03:08:56 +0900

    ReaderWriterLock 구현연습 - WriteLock은 한번에 하나만 ReadLock은 여러개가 동시에 들어갈 수 있음.. 그래서 desired값은 쓰레드아이디로서  고유하게 존재하고 expected값은 하나씩 들어갈 때마다 1씩 증가하도록 코드를 구현하였음

--- SeverCore/Program.cs
+++ SeverCore/Program.cs
@@ -5,6 +5,9 @@ using System.Threading;
 
 namespace SeverCore
 {
+    class Program
+    {
+       
     //메모리 베리어
     // A) 코드 재배치 억제
     // B) 가시성
@@ -15,121 +18,41 @@ namespace SeverCore
 
     // atomic = 원자성
     // SpinLock -> 대기상태에서 무한으로 뺑뺑이 도는 락(무작정 기다리기때문에 CPU점유율이 확 튀는 현상이 발생 할 수 있음...)
-    class Lock
-    {
-        //bool <- 커널
-        AutoResetEvent _available = new AutoResetEvent(true);
 
-        //ManualResetEvent _available_2 = new ManualResetEvent(true); //lock구현시에는 잘 안씀..
+        static int count = 0; 
+        static Lock _lock = new Lock();
 
-        public void Acquire()
-        {
-            _available.WaitOne(); // 입장 시도
-            //_available.Reset(); //bool = false(WaitOne에서 알아서 해줌 Auto)
-
-            //_available_2.WaitOne();
-            //_available_2.Reset(); //문을 닫는다.
-
-        }
-
-        public void Release()
-        {
-            _available.Set();  //flag = true
-
-            //_available_2.Set();
-        }
-    }
-
-    class Program
-    {
-        //락 구현
-        // 1. 한무대기
-        // 2. 양보
-        // 3. 갑질..(event)
-
-        //상호배제
-
-        static int _num = 0;
-        static Lock _lock = new Lock();    
-        static SpinLock _lock_3 = new SpinLock(); // 한무대기
-        static object _lock_4 = new object(); //Monitor
-        // RWLock ReaderWirteLock
-        static ReaderWriterLockSlim _lock_5 = new ReaderWriterLockSlim();
-        //직접 만들기
-
-
-        static Mutex _lock_2 = new Mutex(); //커널동기화객체.. 무겁기도하고, 굳이?라서 게임서버 만들 때는 잘 안쓴다고 함
-        
-        // bool AutoResetEvent 
-        // int ThreadId  Mutex -> 좀 더 비용이 많이 듬
-
-
-        //[][][][]
-        class Reward
-        {
-
-        }
-
-        // 잘 안바뀜.. 그래서 굳이 락을 해야할까? 하더라도 특정 순간에만! -> RWLock 사용
-        static Reward GetRewardById(int id)
-        {
-            _lock_5.EnterReadLock();
-
-
-            _lock_5.ExitReadLock();
-
-            return null;
-        }
-
-        static void AddReward(Reward reward)
-        {
-            _lock_5.EnterWriteLock();
-
-
-            _lock_5.ExitWriteLock();
-        }
-
-        static void Thread_1()
-        {
-            for(int i = 0; i < 10000; i++)
-            {
-                //_lock.Acquire(); //들어가기전 확인 및 들어간 후 락을 잠금
-                //_num++;
-                //_lock.Release(); //끝나고 락을 풀어줌
-
-                _lock_2.WaitOne();
-                _num++;
-                _lock_2.ReleaseMutex();
-            }
-            
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < 10000; i++)
-            {
-                //_lock.Acquire(); //들어가기전 확인 및 들어간 후 락을 잠금
-                //_num--;
-                //_lock.Release(); //끝나고 락을 풀어줌
-
-                _lock_2.WaitOne();
-                _num--;
-                _lock_2.ReleaseMutex();
-            }
-        }
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            Task t1 = new Task(delegate ()
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    _lock.WriteLock();
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                    _lock.WriteUnlock();
+                }
+            });
+
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
 
             t1.Start();
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(_num);
-
+            Console.WriteLine(count);
         }
     }
    
--- SeverCore/obj/Debug/net8.0/SeverCore.AssemblyInfo.cs
+++ SeverCore/obj/Debug/net8.0/SeverCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeverCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55a28fce1966f5e6c4490aeffc0ef743f6e7d07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617f8071c99248fdce74991bd51c499b5863bb9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeverCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeverCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
